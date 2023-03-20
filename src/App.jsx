3b92e5eecba562a801040f54a4b4f65024309bb6
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-20 17:52:20 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <main className="min-h-screen">
+      <header className="hidden">
+        <h1>Testimonials Grid Section Component</h1>
+      </header>
+      <section>
+        <h2 className="hidden">Testimonials</h2>
+        {/* Cards go here */}
+      </section>
+    </main>
+  );
+}
+
+export default App;
