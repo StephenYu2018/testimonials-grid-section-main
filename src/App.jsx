c786bb500f0e48dbee0fcf3bf6aabcbed6809ef8
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-21 10:21:48 -0400

    fix purple card picture border to be more transparent

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function cards() {
       style: {
         gridArea: 'row-span-1 col-span-1',
         backgroundColor: 'bg-moderate-violet',
-        textColor: 'text-light-gray',
+        textColor: 'text-light-gray/30',
         pictureBorderColor: 'border-light-gray'
       }
     }
