490063cb92fa39f0ee29b5921b39690be7bb664c
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-20 20:50:09 -0400

    add spacing and vertically center text to profile

--- src/TestimonialCard.jsx
+++ src/TestimonialCard.jsx
@@ -5,11 +5,11 @@ function TestimonialCard({ content, style }) {
       `${style.gridArea} ${style.backgroundColor} px-[2.462rem] pt-[2rem] 
       pb-[2.462rem]`
     }>
-      <div>
+      <div className="flex flex-row space-x-[1.231rem]">
         <img src={content.pictureURL}
         className={`${style.pictureBorderColor} border-2 w-auto h-auto rounded-full`}
         />
-        <div>
+        <div className="flex flex-col justify-center">
           <h3 className={`${style.textColor} font-barlow`}>{content.fullName}</h3>
           <p className={`${style.textColor} font-barlow`}>Verified Graduate</p>
         </div>
