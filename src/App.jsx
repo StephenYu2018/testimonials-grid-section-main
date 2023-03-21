f881a8b6d9bfc7dfe96259360b791a4c369cf643
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-20 20:00:27 -0400

    overwrite colors to theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import TestimonialCard from "./TestimonialCard";
 
 function App() {
   return (
@@ -7,10 +8,38 @@ function App() {
       </header>
       <section>
         <h2 className="hidden">Testimonials</h2>
-        {/* Cards go here */}
+        {cards().map((card) => 
+          <TestimonialCard content={card.content} style={card.style} />)
+        }
       </section>
     </main>
   );
+}
+
+function cards() {
+  return [
+    {
+      content: {
+        fullName: 'Daniel Clifford',
+        title: `I received a job offer mid-course, and the subjects I learned 
+        were current, if not more so, in the company I joined. I honestly feel 
+        I got every penny’s worth.`,
+        quote: `“ I was an EMT for many years before I joined the bootcamp. 
+        I’ve been looking to make a transition and have heard some people who 
+        had an amazing experience here. I signed up for the free intro course 
+        and found it incredibly fun! I enrolled shortly thereafter. The next 
+        12 weeks was the best - and most grueling - time of my life. Since 
+        completing the course, I’ve successfully switched careers, working as 
+        a Software Engineer at a VR startup. ”`
+      },
+      style: {
+        gridArea: 'row-span-1 col-span-1',
+        backgroundColor: 'bg-moderate-violet',
+        textColor: 'text-light-gray',
+        pictureBorderColor: 'border-light-gray'
+      }
+    }
+  ];
 }
 
 export default App;
