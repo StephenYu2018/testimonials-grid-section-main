fe74c53d251bf47bdbb7f025b5fdcab74d7cb898
StephenYu2018 <stephenyu8021@gmail.com>
2023-03-22 13:43:07 -0400

    add grid gap-x back for xl screen breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
       <section className="grid
       grid-rows-[27.75rem_14.875rem_14rem_24rem_27.375rem]
       xl:grid-rows-[17.625rem_16.625rem] grid-cols-[20.25rem]
-      xl:grid-cols-[repeat(4,_15.9375rem)] gap-y-6 xl:gap-y-[1.4375rem]">
+      xl:grid-cols-[repeat(4,_15.9375rem)] xl:gap-x-[1.8125rem]
+      gap-y-6 xl:gap-y-[1.4375rem]">
         <h2 className="hidden">Testimonials</h2>
         {cards.map(card => 
           <TestimonialCard key={card.content.fullName}
